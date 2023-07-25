0459d2ba635c95f4ae391bd8dbfb87ce5ff31c42
gopinath <gopinathkaiser@gmail.com>
2023-07-25 14:50:13 +0530

    changed variables and functions

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let eid = document.getElementById("employeeid");
+let eid = document.getElementById("id");
 let ename = document.getElementById("name");
 let edesignation = document.getElementById("designation");
 let esal = document.getElementById("salary");
@@ -8,7 +8,6 @@ let employeeDetails = new Array();
 
 
 function getDetails() {
-   // event.preventDefaultult();
    if(eid.value == "" || ename.value == "" || edesignation.value == "" || esal.value == "" || ecity.value == ""){
         alert("Fill all the fields");
    }else{
@@ -19,25 +18,22 @@ function getDetails() {
 }
 
 
-function loading(){
+function onRefresh(){
      
     let tempArray = new Array();
     
     for (let employee in localStorage){
         
-        s = JSON.parse(JSON.stringify(localStorage.getItem(employee)));
-        if(s==null){
+        let jsonString = JSON.parse(JSON.stringify(localStorage.getItem(employee)));
+        if(jsonString==null){
             break;
         }
-        tempArray = s.split(",");
+        tempArray = jsonString.split(",");
         employeeDetails.push(tempArray);
 
-        // console.log("local storage " + arr1);
         
     }
-    display();
-    // arr1 = s.split(",");
-    // console.log("local storage " + arr1);
+    displayEmployees();
 
 }
 
@@ -54,8 +50,7 @@ function addEmployee() {
     localStorage.setItem(key,tempArray);
     
     employeeDetails.push(tempArray);
-    // console.log("local item" + employeeDetails);
-    display();
+    displayEmployees();
     document.getElementById("employeeid").value = "";
     document.getElementById("name").value = "";
     document.getElementById("designation").value = "";
@@ -65,7 +60,7 @@ function addEmployee() {
 
 }
 
-function display() {
+function displayEmployees() {
 
     
     document.getElementById("table-main").innerHTML = "<tr><th> Employee ID </th> <th> NAME </th> <th>DESIGNATION</th> <th>SALARY</th> <th>CITY</th> <th>ACTIONS</th> </tr>";
@@ -75,9 +70,8 @@ function display() {
         
 
 
-        document.getElementById("table-main").innerHTML += `<tr><td>  ${tempArray[0]} </td><td> ${tempArray[1]} </td> <td> ${tempArray[2]} </td><td> ${tempArray[3]} </td> <td> ${tempArray[4]} </td><td> <a href = "#" onclick = "edit(${tempArray[0]})">Edit</a>  <a href = "#" onclick="deleteRow(${tempArray[0]})">Delete </a></td></tr>`;
+        document.getElementById("table-main").innerHTML += `<tr><td>  ${tempArray[0]} </td><td> ${tempArray[1]} </td> <td> ${tempArray[2]} </td><td> ${tempArray[3]} </td> <td> ${tempArray[4]} </td><td> <a href = "#" onclick = "editEmployeeList(${tempArray[0]})">Edit</a>  <a href = "#" onclick="deleteEmployeeList(${tempArray[0]})">Delete </a></td></tr>`;
         
-        //console.log(main);
     });
 
 
@@ -92,7 +86,7 @@ function visibleForm() {
 
 }
 
-function edit(val) {
+function editEmployeeList(val) {
 
     document.getElementById('form-data-update').style.display = "block";
     document.getElementById('form-data').style.display = "none";
@@ -114,11 +108,9 @@ function edit(val) {
     document.getElementById("employeeDesignationUpdate").value = updatedArr[2];
     document.getElementById("employeeSalaryUpdate").value = updatedArr[3];
     document.getElementById("employeeCityUpdate").value = updatedArr[4];
-    // globalValue = ++val;
-    //console.log(newArr);
 }
 
-function update() {
+function updateEmployeeDetails() {
     let eidUpdate = document.getElementById("employeeIdUpdate");
     let enameUpdate = document.getElementById("employeeNameUpdate");
     let edesignationUpdate = document.getElementById("employeeDesignationUpdate");
@@ -132,37 +124,30 @@ function update() {
     changedValuesArray.push(esalUpdate.value);
     changedValuesArray.push(ecityUpdate.value);
 
-    // let temp = --globalValue;
+
     let tempArray = new Array();
     let index;
     for(let i=0;i<employeeDetails.length;i++){
         tempArray = employeeDetails[i];
-        //console.log("array" + arr2[0] +"e id value" + eid1.value);
         if(tempArray[0] == eidUpdate.value){
             index = i;
             break;
         }
     }
 
-    // console.log("main before update " + main + "index" + index);
     employeeDetails[index] = changedValuesArray;
-    // console.log("main after update " + main);
-   // console.log(main[temp]);
    localStorage.setItem(eidUpdate.value,changedValuesArray);
-    display();
+   displayEmployees();
     document.getElementById('form-data-update').style.display = "none";
 }
 
-function deleteRow(val) {
+function deleteEmployeeList(val) {
     console.log(employeeDetails);
     if(!confirm("Are u sure")){
         return ;
     }
      
-    // console.log("index " + val);
     let tempArray = new Array();
-   // arr2 = main[val];
-    //console.log(main);
     
     let index;
     for(let i=0;i<employeeDetails.length;i++){
@@ -172,10 +157,9 @@ function deleteRow(val) {
             break;
         }
     }
-    // console.log("array to be deleted :" + employeeDetails[index] + "val is" + val + "index is " + index);
     employeeDetails.splice(index, 1);
      localStorage.removeItem(val);
-    display();
+     displayEmployees();
 }
 
 
